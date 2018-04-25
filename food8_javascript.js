15a1965642d8ccb63e9914df6e518425a3e5e5c4
mind0004 <mind0004@stud.kea.dk>
2018-04-25 17:50:07 +0200

    asd

--- food8_javascript.js
+++ food8_javascript.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", hentJson);
 async function hentJson() {
 
     //hent Json0
-    let jsonObjekt = await fetch("http://mikkelhein.dk/kea/food8/wordpress/wp-json/wp/v2/posts")
+    let jsonObjekt = await fetch("http://mikkelhein.dk/kea/food8/wordpress/wp-json/wp/v2/shortposts")
 
     //vis objekt som json
     posts = await jsonObjekt.json();
@@ -25,14 +25,15 @@ function visPosts() {
     posts.forEach(element => {
 
         let klon = minTemplate.cloneNode(true).content;
+        let nummer = Number(-60);
 
         klon.querySelector("[data-navn]").innerHTML = element.title.rendered;
         //klon.querySelector("[data-type]").textContent = element.acf.fugletype;
-        klon.querySelector("[data-tekst]").innerHTML = element.excerpt.rendered;
+        klon.querySelector("[data-tekst]").innerHTML = element.content.rendered;
         //klon.querySelector("[data-billede]").src = element.featured_media;
 
-        klon.querySelector("[data-single]").href = "single.html?restaurant=" + element.id;
-        klon.querySelector("[data-readmore]").href = "single.html?restaurant=" + element.id;
+        klon.querySelector("[data-single]").href = "single.html?restaurant=" + (element.id + nummer);
+        klon.querySelector("[data-readmore]").href = "single.html?restaurant=" + (element.id + nummer);
 
         document.querySelector("#modtager").appendChild(klon);
 
